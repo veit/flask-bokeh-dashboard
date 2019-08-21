2d3300536ff8fa4f9a2047b0117cdd4766c28171
veit <kontakt@veit-schiele.de>
2019-08-21 11:09:16 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,102 @@
+===============================================
+Material Dashboard with Bokeh embedded in Flask
+===============================================
+
+Features
+========
+
+The package provides a starter pack with an interactive `Bokeh
+<https://bokeh.pydata.org>`_ plot embedded in an `Material Design
+<https://material.io/>`_ Dashboard, which can send parameters from a flask form
+to Bokeh.
+
+.. image:: screenshot.png
+   :scale: 53%
+   :alt: Material Dashboard with Bokeh embedded in Flask
+
+Quickstart
+==========
+
+Before you install Python packages, you must meet a few requirements.
+
+#. Make sure you use the desired Python version:
+
+   .. code-block:: console
+
+    $ python --version
+    Python 3.7.3
+
+    Only Python >=3.6 is supported.
+
+#. Make sure that `Pip
+   <https://jupyter-tutorial.readthedocs.io/de/latest/reproduce/packaging/glossary.html#term-pip>`_
+   is installed:
+
+   .. code-block:: console
+
+    $ pip --version
+    pip 18.1
+
+#. If Pip isn’t installed, you can install it with:
+
+   .. code-block:: console
+
+    $ sudo apt install python3-venv python3-pip
+
+#. Install `Pipenv <https://docs.pipenv.org>`_:
+
+   .. code-block:: console
+
+    $ pip3 install --user pipenv
+      Downloading pipenv-2018.7.1-py3-none-any.whl (5.0MB): 5.0MB downloaded
+    Requirement already satisfied (use --upgrade to upgrade): virtualenv in /usr/lib/python3/dist-packages (from pipenv)
+    Installing collected packages: pipenv, certifi, pip, setuptools, virtualenv-clone
+    …
+    Successfully installed pipenv certifi pip setuptools virtualenv-clone
+    Cleaning up...
+
+#. Download:
+
+   .. code-block:: console
+
+    $ curl -O https://github.com/veit/flask-bokeh-dashboard/archive/master.zip
+    $ unzip master.zip
+
+#. Create virtual environment:
+
+   .. code-block:: console
+
+    $ cd flask-bokeh-dashboard
+    $ pipenv install
+    Creating a virtualenv for this project…
+    …
+    Updated Pipfile.lock 
+
+    Installing dependencies from Pipfile.lock Updated Pipfile.lock (f042ee)…
+    …
+
+#. Run the dashboard with the `gunicorn
+   <http://docs.gunicorn.org/en/stable/run.html>`_ command:
+
+   .. code-block:: console
+
+    $ pipenv run gunicorn -w 1 main:app
+    [2019-08-21 10:14:54 +0200] [3750] [INFO] Starting gunicorn 19.9.0
+    [2019-08-21 10:14:54 +0200] [3750] [INFO] Listening at: http://127.0.0.1:8000 (3750)
+    [2019-08-21 10:14:54 +0200] [3750] [INFO] Using worker: sync
+    [2019-08-21 10:14:54 +0200] [3757] [INFO] Booting worker with pid: 3757
+
+   .. note::
+      The ``w`` option can be used to specify the number of workers.
+
+#. Visit http://127.0.0.1:8000 and it should look like the screenshot above.
+
+#. You can shut down the service in the console with ``ctrl-c``.
+
+Pull requests
+=============
+
+If you have differences in your preferred setup, I encourage you to fork this
+to create your own version. I also accept pull requests on this, if they’re
+small, atomic, and if they make my own packaging experience better.
+
---
+++ main.py
@@ -0,0 +1,103 @@
+try:
+    import asyncio
+except ImportError:
+    raise RuntimeError("This requries at least Python3.4/asyncio")
+
+from flask import Flask, render_template, request
+
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+
+from bokeh.application import Application
+from bokeh.application.handlers import FunctionHandler
+from bokeh.embed import server_document
+from bokeh.layouts import column
+from bokeh.models import ColumnDataSource, Slider
+from bokeh.plotting import figure
+from bokeh.server.server import BaseServer
+from bokeh.server.tornado import BokehTornado
+from bokeh.server.util import bind_sockets
+from bokeh.themes import Theme
+from bokeh.sampledata.sea_surface_temperature import sea_surface_temperature
+
+if __name__ == '__main__':
+    print('This script is intended to be run with gunicorn. e.g.')
+    print()
+    print('    gunicorn -w 4 app:app')
+    print()
+    print('will start the app on four processes')
+    import sys
+    sys.exit()
+
+app = Flask(__name__)
+
+def modify_doc(doc):
+    flask_args = doc.session_context.request.arguments
+    unit_type = flask_args.get('unit_type')[0].decode("utf-8")
+
+    df = sea_surface_temperature.copy()
+    source = ColumnDataSource(data=df)
+    plot = figure(x_axis_type='datetime',
+                  y_range=(0, 25), 
+                  y_axis_label=f"Temperature ({unit_type})" 
+                  )
+    plot.line('time', 'temperature', source=source)
+
+    def callback(attr, old, new):
+        if new == 0:
+            data = df
+        else:
+            data = df.rolling('{0}D'.format(new)).mean()
+        source.data = ColumnDataSource(data=data).data
+
+    slider = Slider(start=0, end=30, value=0, step=1, title="Smoothing by N Days")
+    slider.on_change('value', callback)
+
+    doc.add_root(column(slider, plot))
+    doc.theme = Theme(filename="theme.yaml")
+
+bkapp = Application(FunctionHandler(modify_doc))
+
+# This is so that if this app is run using something like "gunicorn -w 4" then
+# each process will listen on its own port
+sockets, port = bind_sockets("127.0.0.1", 0)
+
+@app.route('/', methods=['GET'])
+def bkapp_page():
+    dataset = request.args.get('dataset')
+
+    if request.args.get('unit_type') is None:
+      unit_type = "Celcius"
+    else:
+      unit_type = request.args.get('unit_type')
+    
+    script = server_document(
+      'http://127.0.0.1:%d/bkapp' % port, 
+      arguments=dict(
+      unit_type = unit_type)
+    )
+
+    return render_template(
+      "index.html", 
+      script = script,
+      app_name = "Material Dashboard with Bokeh embedded in Flask", 
+      app_description = "This Dashboard is served by a Bokeh server embedded in Flask.",
+      app_icon = "timeline",
+      unit_type = unit_type,
+      dataset = dataset
+      )
+
+def bk_worker():
+    asyncio.set_event_loop(asyncio.new_event_loop())
+
+    bokeh_tornado = BokehTornado({'/bkapp': bkapp}, extra_websocket_origins=["127.0.0.1:8000"])
+    bokeh_http = HTTPServer(bokeh_tornado)
+    bokeh_http.add_sockets(sockets)
+
+    server = BaseServer(IOLoop.current(), bokeh_tornado, bokeh_http)
+    server.start()
+    server.io_loop.start()
+
+from threading import Thread
+Thread(target=bk_worker).start()
+
